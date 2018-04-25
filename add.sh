d9da007d3435f9b3cc2033ccce5147fb1fbd4766
annprog <tecbbs@qq.com>
2018-04-25 18:02:17 +0800

    finish addModule

--- add.sh
+++ add.sh
@@ -25,5 +25,13 @@ function ngxTemplate() {
 	sed -i 's/## start server /server {\nlisten 8000;\nallow 10.0.0.0\/8;\nallow 127.0.0.0\/8;\ndeny all;\nlocation \/reqstat {\nreq_status_show letvapp;\n}\n}\n## start server /g' $location
 }
 
+function addModule() {
+	location="ingress-nginx/images/nginx/build.sh"
+	hash256="96ac3dee89dc85dbd97e414e5c91be4bc6fc36e4dbaba53b09b4fa02874fd117"
+	sed -i -r "s/(^# download, verify and extract the source files)/\1\nget_src 96ac3dee89dc85dbd97e414e5c91be4bc6fc36e4dbaba53b09b4fa02874fd117 \"https:\/\/github.com\/annProg\/ngx_http_reqstat\/archive\/master.zip\"/g" $location
+	sed -i 's/^WITH_MODULES="/WITH_MODULES="--add-module=$BUILD_PATH\/ngx_http_reqstat-master /g' $location
+}
+
 init
 ngxTemplate
+addModule
