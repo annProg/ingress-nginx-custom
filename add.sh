bd1e0ed98adf524f12a4b642a007d462323b387c
annprog <tecbbs@qq.com>
2018-04-26 16:58:29 +0800

    fix doc

--- add.sh
+++ add.sh
@@ -61,7 +61,7 @@ function compile() {
 
 	title "3. Compile ingress-contorller"
 	echo "cd $DIR_INGRESS"
-	echo "ARCH=amd64 BASEIMAGE=quay.io/kubernetes-ingress-controller/nginx-amd64:reqstat make docker-build"
+	echo "ARCH=amd64 BASEIMAGE=quay.io/kubernetes-ingress-controller/nginx-amd64:reqstat make container"
 }	
 
 init
